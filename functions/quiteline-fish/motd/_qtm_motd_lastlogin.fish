98919bb551ae8c273c0be8108ccf9d609e7089c1
qwreey <me@qwreey.moe>
2026-06-08 18:15:55 +0900

    fix(motd): query command first to avoid 'command last not found' error

--- functions/quiteline-fish/motd/_qtm_motd_lastlogin.fish
+++ functions/quiteline-fish/motd/_qtm_motd_lastlogin.fish
@@ -8,7 +8,8 @@ function _qtm_motd_lastlogin; argparse --max-args 0 \
 	set -q _flag_locale || set -l _flag_locale "$LC_TIME"
 	test -z _flag_locale && set -l _flag_locale "en_US.UTF-8"
 
-	set -l last_timeiso (string match -rg '(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})' (last -n 1 --time-format=iso $USER)[1])
+	command -q last
+	and set -l last_timeiso (string match -rg '(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})' (last -n 1 --time-format=iso $USER)[1])
 	or set -l last_timeiso (date -Iseconds)
 	set -l formatted (LC_TIME="$_flag_locale" date -d $last_timeiso "+$_flag_dateformat")
 
