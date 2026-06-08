d5f2de3732d900cdaafa5711c52faa85f2553847
qwreey <me@qwreey.moe>
2026-06-09 01:20:41 +0900

    feat(motd): add random charactor picking when more than one selected

--- functions/quiteline-fish/motd/_qtm_motd.fish
+++ functions/quiteline-fish/motd/_qtm_motd.fish
@@ -5,7 +5,8 @@ function _qtm_motd; argparse --max-args 0 \
 	'line3=*' \
 -- $argv
 	# Define default
-	set -q _flag_charactor || set -l _flag_charactor "cat"
+	set -q _flag_charactor || set -l _flag_charactor "cat" "rabbit"
+	set _flag_charactor $_flag_charactor[(random 1 (count $_flag_charactor))]
 
 	# Create lines
 	set -l line1 (string join '' $_flag_line1)
